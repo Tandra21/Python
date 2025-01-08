6a388c877931b80ad0cde3034f06c72830b67b6e
Tandra Pramanik <156818552+Tandra21@users.noreply.github.com>
2025-01-08 15:09:57 +0600

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,12 @@
+name="Reba"
+print("Hi,"+name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+#loop
+mango='''I am Tandra 
+"I want to eat a mango'''
+print("Lets use a for loop\n")
+for character in mango:
+    print(character)
