f85365e42ef4e8aa6d03f1ee5ca81a6970a263cd
Tandra Pramanik <156818552+Tandra21@users.noreply.github.com>
2025-03-25 21:40:57 +0600

    Add files via upload

---
+++ nested-if.py
@@ -0,0 +1,12 @@
+r=-1
+if(r<0):
+      print("true")
+elif(r>0):
+    if(r<=10):
+        print("number between 1-10")
+    elif(r>10 and r<=20):
+        print("number between 11-20")
+    else:
+        print("number greater than 20")
+else:
+     print("number is zero")
