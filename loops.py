13330980feff1938d0827f9094080f3e4f754067
Tandra Pramanik <156818552+Tandra21@users.noreply.github.com>
2025-03-26 19:48:18 +0600

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,17 @@
+#name='Reba'
+#for i in name:
+ #print(i)
+#if(i=="n"):
+# print("This is true!")
+#colors = ["Red","Green","blue","Yellow"]
+#for i in colors:
+    #print(colors)
+    ##print(i)
+#for k in range(5):
+ #print(k+1)
+
+#for k in range(1,20001):
+  #print(k)
+  
+for k in range(1, 12, 3):
+    print(k)
